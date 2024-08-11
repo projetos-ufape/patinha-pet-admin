26259dcdb9e6a1b21ddd628036aaeb6260a7b4db
Luanzacarias <luanzacarias215@gmail.com>
2024-08-11 10:31:37 -0300

    feat: setUp inicial e estrutura da tabela

---
+++ app/Models/Pet.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Pet extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'nome',
+        'gender',
+        'specie',
+        'race',
+        'height', 
+        'weight',
+        'birth'
+    ];
+}
---
+++ database/migrations/2024_08_11_014113_create_pets_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pets', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->enum('gender', ['female', 'male']);
+            $table->enum('specie', ['cat', 'dog']);
+            $table->string('race');
+            $table->float('height', 8, 2);
+            $table->float('weight', 8, 2);
+            $table->date('birth');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pets');
+    }
+};
