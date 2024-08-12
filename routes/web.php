9483b8c3b91dd59f91ef42a36c26dca2cb4896f7
Gustavo Barbosa <gustavobarbosa-18@hotmail.com>
2024-08-11 22:49:47 -0300

    feat: adicionar resource de produtos

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,6 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+Route::resource('products', App\Http\Controllers\ProductController::class);
+
+require __DIR__ . '/auth.php';
