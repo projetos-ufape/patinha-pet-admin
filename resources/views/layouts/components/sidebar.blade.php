0ebef13198ede0756696d90a68afbde06e8925d8
AnaliceBattisti <analicebattistii@gmail.com>
2024-08-13 14:11:15 -0300

    Criação sidebar

---
+++ resources/views/layouts/components/sidebar.blade.php
@@ -0,0 +1,44 @@
+<div id="sidebar" class="shadow-lg border-gray-200 p-4 hidden transition-opacity opacity-0" style="background: var(--Container, #F2F6F7);  width: 250px ;">
+    <ul class="space-y-6">
+
+        <li class="hover:bg-gray-200 rounded-lg flex items-center p-2">
+            <img src="{{ asset('icons/sidebar/customer.svg') }}" alt="Dashboard Icon" class="h-5 w-5">
+            <a href="{{ route('customer.index') }}" class="text-gray-700 hover:text-gray-900 block ml-2">Clientes</a>
+        </li>
+
+        <li class="hover:bg-gray-200 rounded-lg flex items-center p-2">
+            <img src="{{ asset('icons/sidebar/pets.svg') }}" alt="Dashboard Icon" class="h-5 w-5">
+            <a href="#" class="text-gray-700 hover:text-gray-900 block ml-2">Pets</a>
+        </li>
+
+        <li class="hover:bg-gray-200 rounded-lg flex items-center p-2">
+            <img src="{{ asset('icons/sidebar/products.svg') }}" alt="Dashboard Icon" class="h-5 w-5">
+            <a href="#" class="text-gray-700 hover:text-gray-900 block ml-2">Produtos</a>
+        </li>
+        
+        <li class="hover:bg-gray-200 rounded-lg flex items-center p-2">
+            <img src="{{ asset('icons/sidebar/services.svg') }}" alt="Dashboard Icon" class="h-5 w-5">
+            <a href="#" class="text-gray-700 hover:text-gray-900 block ml-2">Serviços</a>
+        </li>
+
+        <li class="hover:bg-gray-200 rounded-lg flex items-center p-2">
+            <img src="{{ asset('icons/sidebar/stock.svg') }}" alt="Dashboard Icon" class="h-5 w-5">
+            <a href="#" class="text-gray-700 hover:text-gray-900 block ml-2">Estoque</a>
+        </li>
+        
+    </ul>
+</div>
+
+
+<script>
+
+    const menuButton = document.getElementById('menu-button');
+    const sidebar = document.getElementById('sidebar');
+    menuButton.addEventListener('click', () => {
+        sidebar.classList.toggle('hidden');
+        setTimeout(() => {
+            sidebar.classList.toggle('opacity-0');
+        }, 20);
+    });
+
+</script>
