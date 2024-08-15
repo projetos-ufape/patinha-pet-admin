112016021db2f7e29e943141054d15499452a865
Capeudinho <archmagepyromancer@gmail.com>
2024-08-15 17:43:06 -0300

    Atualização de controller de registro

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -2,13 +2,16 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Enums\AddressState;
 use App\Http\Controllers\Controller;
+use App\Models\Address;
 use App\Models\User;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rule;
 use Illuminate\Validation\Rules;
 use Illuminate\View\View;
 
@@ -33,12 +36,36 @@ class RegisteredUserController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
+			'cpf' => ['required', 'string'],
+			'salary' => ['required', 'numeric'],
+			'admission_date' => ['required', 'date', 'date_format:Y-m-d'],
+			'state' => ['required', Rule::enum(AddressState::class)],
+			'city' => ['required', 'string'],
+			'district' => ['required', 'string'],
+			'street' => ['required', 'string'],
+			'number' => ['required', 'integer'],
+			'complement' => ['string'],
+			'cep' => ['required', 'string'],
         ]);
 
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+			'cpf' => $request->cpf,
+			'salary' => $request->salary,
+			'admission_date' => $request->admission_date,
+        ]);
+
+		Address::create([
+            'state' => $request->state,
+            'city' => $request->city,
+			'district' => $request->district,
+			'street' => $request->street,
+			'number' => $request->number,
+			'cep' => $request->cep,
+			'complement' => $request->complement,
+			'user_id' => $user->id,
         ]);
 
         event(new Registered($user));
