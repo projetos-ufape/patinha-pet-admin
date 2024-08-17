719acee6a974de435c8e96d1d9f6b2b11113d0ca
marco_antonio <marcoantoniomoraes760@gmail.com>
2024-08-17 16:46:59 -0300

    feat: Criar e modelar entidade de cliente

---
+++ app/Models/Customer.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Customer extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'cpf',
+        'phone_number',
+        'email',
+        'password'
+    ];
+
+    protected $hidden = [
+        'password',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'password' => 'hashed',
+        ];
+    }
+
+    public function address()
+    {
+        // return $this->hasOne(Address::class, 'customer_id');
+    }
+
+}
