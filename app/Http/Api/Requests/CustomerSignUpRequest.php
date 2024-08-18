78827762264adebf2bba39d0a076a7f9d5ae5a15
Gustavo Barbosa <gustavobarbosa-18@hotmail.com>
2024-08-18 18:42:02 -0300

    feat: adicionar autenticação do cliente no app mobile

---
+++ app/Http/Api/Requests/CustomerSignUpRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Api\Requests;
+
+use App\Enums\AddressState;
+use App\Models\User;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+use Illuminate\Validation\Rules;
+
+class CustomerSignUpRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:' . User::class],
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'cpf' => ['required', 'string'],
+            'phone_number' => ['required', 'numeric', 'digits:11'],
+            'address' => ['sometimes', 'array'],
+            'address.cep' => ['sometimes', 'size:8'],
+            'address.street' => ['required_with:address', 'min:3', 'max:50'],
+            'address.number' => ['required_with:address', 'string', 'min:1', 'max:10'],
+            'address.district' => ['required_with:address', 'min:3', 'max:50'],
+            'address.city' => ['required_with:address', 'min:3', 'max:50'],
+            'address.complement' => ['string', 'max:50'],
+            'address.state' => ['required_with:address', Rule::enum(AddressState::class)],
+        ];
+    }
+}
