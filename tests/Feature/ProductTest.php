4fc76621277c0b5925d1067994740c22aa40d03c
marianeme <marianedmsilva@gmail.com>
2024-08-19 01:47:51 -0300

    Atualiza feature tests de Product

--- tests/Feature/ProductTest.php
+++ tests/Feature/ProductTest.php
@@ -1,32 +1,43 @@
 <?php
 
 use App\Models\Product;
-use Illuminate\Testing\Fluent\AssertableJson as FluentAssertableJson;
+use App\Models\User;
+
 
 test('list of products is displayed', function (){
-    $product = Product::factory()->create(); 
+    $admin = User::factory()->create();
+    $products = Product::factory()->count(3)->create();
 
-    $response = $this->get('/products');
-    $response->assertOk(); 
-    $response->assertJson(fn (FluentAssertableJson $json) => 
-    $json->has('data') 
-        ->where('data.0.id', $product->id) 
-        ->etc()
-);
+    $response = $this->actingAs($admin, 'web')
+        ->get(route('products.index'));
+
+    foreach ($products as $product) {
+        $this->assertDatabaseHas('products', [
+            'name' => $product->name,
+            'description' => $product->description,
+            'brand' => $product->brand,
+            'category' => $product->category,
+            'price' => $product->price,
+            'quantity' => $product->quantity,
+        ]);
+    }
+    $response->assertStatus(200);
 });
 
 test('list of products is empty when no products have been created', function () {
-    $response = $this->get('/products');
-    $response->assertOk();
-    $response->assertJson([
-        'data' => []
-    ]);
+    $admin = User::factory()->create();
+    $response = $this->actingAs($admin, 'web')
+        ->get(route('products.index'));
+        
+    $this->assertDatabaseCount('products', 0);
 });
 
-test('product info was successfully updated', function () {
+test('admin can update existing product info', function () {
+    $admin = User::factory()->create();
     $product = Product::factory()->create(); 
 
-    $response = $this->patch('/product/'.$product->id, ['name' => 'Dog Bed',
+    $response = $this->actingAs($admin, 'web')
+        ->patch('/products/'.$product->id, ['name' => 'Dog Bed',
         'description' => 'Comfortable blue checkered medium sized dog bed.',
         'brand' => 'Wild',
         'category' => 'toy',
@@ -36,31 +47,43 @@ test('product info was successfully updated', function () {
     $updatedProduct = Product::find($product->id);
     $this->assertEquals('Dog Bed', $updatedProduct->name);
     $this->assertEquals('Wild', $updatedProduct->brand);
+    $this->assertEquals('toy', $updatedProduct->category);
+    $this->assertEquals('50.00', $updatedProduct->price);
+    $this->assertEquals('2', $updatedProduct->quantity);
    
-    $response->assertStatus(200);
-    $response->assertJson([
-        'name' => 'Dog Bed',
-        'brand' => 'Wild'
-        ]);
-
-    //$response->assertRedirect('/products');
+    $response
+        ->assertStatus(302)
+        ->assertRedirect(route('products.index'))
+        ->assertSessionHas('success', 'Produto atualizado com sucesso.');
+    
 });
 
+test('admin cannot update non-existing product info', function () {
+    $admin = User::factory()->create();
+    $response = $this->actingAs($admin, 'web')
+        ->patch('/products/33', ['name' => 'Dog Bed',
+        'description' => 'Comfortable blue checkered medium sized dog bed.',
+        'brand' => 'Wild',
+        'category' => 'toy',
+        'price' => 50.00,
+        'quantity' => 2,]);
+    $response
+        ->assertStatus(404);
+});
 
-test('show specific product information successfully', function () {
+test('shows specific product information successfully', function () {
+    $admin = User::factory()->create();
 	$product = Product::factory()->create();
 
-    $response = $this->get('/product/'.$product->id);
-
+    $response = $this->actingAs($admin, 'web')
+        ->get('/products/'.$product->id);
 	$response->assertStatus(200);
-	$response->assertJson([
-		'id' => $product->id,
-	]);
 });
 
-test('adding a product w/ invalid category', function () {
-
-    $response = $this->post('/products', [
+test('admin cannot add a product w/ invalid category', function () {
+    $admin = User::factory()->create();
+    $response = $this->actingAs($admin, 'web')
+        ->post('/products', [
         'name' => 'Dog Bed',
         'description' => 'Comfortable blue checkered medium sized dog bed.',
         'brand' => 'Wild',
@@ -72,9 +95,10 @@ test('adding a product w/ invalid category', function () {
     $response->assertInvalid(['category' => 'The selected category is invalid.']);
 });
 
-test('adding a product w/ invalid price', function () {
-
-    $response = $this->post('/products', [
+test('admin cannot add a product w/ invalid price', function () {
+    $admin = User::factory()->create();
+    $response = $this->actingAs($admin, 'web')
+        ->post('/products', [
         'name' => 'Dog Bed',
         'description' => 'Comfortable blue checkered medium sized dog bed.',
         'brand' => 'Wild',
@@ -86,21 +110,42 @@ test('adding a product w/ invalid price', function () {
     $response->assertInvalid(['price' => 'The price field format is invalid.']);
 });
 
-test('destroy existing product', function () {
+test('admin can destroy existing product', function () {
+    $admin = User::factory()->create();
 	$product = Product::factory()->create();
 
-    $response = $this->delete('/product/'.$product->id);
+    $response = $this->actingAs($admin, 'web')
+        ->delete('/products/'.$product->id);
 
 	$createdProduct = Product::find($product->id);
 
-	$response->assertStatus(200);
-	$response->assertJson([
-		'id' => $product->id,
-	]);
+	$response
+        ->assertStatus(302)
+        ->assertRedirect(route('products.index'))
+        ->assertSessionHas('success', 'Produto removido com sucesso.');
+
 	$this->assertEquals(null, $createdProduct);
+    $this->assertDatabaseMissing('products', [
+        'id' => $product->id,
+    ]);
 });
 
-test('destroy non-existing product', function () {
-    $response = $this->delete('/product/33');
+test('admin cannot destroy non-existing product', function () {
+    $admin = User::factory()->create();
+    $response = $this->actingAs($admin, 'web')
+        ->delete('/products/33');
     $response->assertStatus(500);
+});
+
+test('non-admin cannot update product list', function () {
+    $product = Product::factory()->create(); 
+    $response = $this->patch('/products/'.$product->id, ['name' => 'Dog Bed',
+        'description' => 'Comfortable blue checkered medium sized dog bed.',
+        'brand' => 'Wild',
+        'category' => 'toy',
+        'price' => 50.00,
+        'quantity' => 2,]);
+    $response
+        ->assertStatus(302)
+        ->assertRedirect(route('login'));
 });
