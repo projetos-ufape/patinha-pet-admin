12f5243e8fe656fb4cbfbe8162a5a71a26cf9d3c
marianeme <marianedmsilva@gmail.com>
2024-08-31 00:50:51 -0300

    Atualiza form requests para validação de criação e atualização de agendamentos

---
+++ app/Http/Requests/StoreAppointmentRequest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Enums\AppointmentStatus;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class StoreAppointmentRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    //public function authorize(): bool
+    //{
+     //   return false;
+    //}
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'employee_id' => ['required', 'exists:employees,id'],
+            'customer_id' => ['required', 'exists:customers,id'],
+            'pet_id' => ['required', 'exists:pets,id'],
+            'service_id' => ['required', 'exists:services,id'],
+            'status' => [Rule::enum(AppointmentStatus::class)],
+            'start_time' => ['required','date_format:Y-m-d H:i:s'],
+            'end_time' => ['nullable','date_format:Y-m-d H:i:s'],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'employee_id.required' => 'O campo de funcionário é obrigatório.',
+            'employee_id.exists' => 'O campo de funcionário deve ser preenchido por um id válido.',
+            'customer_id.required' => 'O campo de cliente é obrigatório.',
+            'customer_id.exists' => 'O campo de cliente deve ser preenchido por um id válido.', 
+            'pet_id.required' => 'O campo de pet é obrigatório.',
+            'pet_id.exists' => 'O campo de pet deve ser preenchido por um id válido.',
+            'service_id.required' => 'O campo de serviço é obrigatório.',
+            'service_id.exists' => 'O campo de serviço deve ser preenchido por um id válido.',
+            'status' => 'O status do atendimento deve ser "pendente", "concluído" ou "cancelado".',
+            'start_time.required' => 'O campo de horário do atendimento é obrigatório.',
+            'start_time'=> 'O horário para o atendimento deve ter o formato Y-m-d H:i:s.',
+            'end_time'=>'O horário para conclusão do atendimento deve ter o formato Y-m-d H:i:s.',
+        ];
+    }
+}
---
+++ app/Http/Requests/UpdateAppointmentRequest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Enums\AppointmentStatus;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class UpdateAppointmentRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    //public function authorize(): bool
+    //{
+    //  return false;
+    //}
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'employee_id' => ['exists:employees,id'],
+            'customer_id' => ['exists:customers,id'],
+            'pet_id' => ['exists:pets,id'],
+            'service_id' => ['exists:services,id'],
+            'status' => [Rule::enum(AppointmentStatus::class)],
+            'start_time' => ['date_format:Y-m-d H:i:s'],
+            'end_time' => ['nullable', 'date_format:Y-m-d H:i:s'],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'employee_id.exists' => 'O campo de funcionário deve ser preenchido por um id válido.', 
+            'customer_id.exists' => 'O campo de cliente deve ser preenchido por um id válido.', 
+            'pet_id.exists' => 'O campo de pet deve ser preenchido por um id válido.',
+            'service_id.exists' => 'O campo de serviço deve ser preenchido por um id válido.',
+            'status' => 'O status do atendimento deve ser "pendente", "concluído" ou "cancelado".',
+            'start_time'=> 'O horário para o atendimento deve ter o formato Y-m-d H:i:s.',
+            'end_time'=> 'O horário para conclusão do atendimento deve ter o formato Y-m-d H:i:s.',
+        ];
+    }
+}
