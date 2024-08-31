35dc1d39dfa66b2a04c4588fa69f05ae611d6c82
marianeme <marianedmsilva@gmail.com>
2024-08-31 00:40:52 -0300

    Adiciona model de agendamento

---
+++ app/Models/Appointment.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Appointment extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'employee_id',
+        'customer_id',
+        'pet_id',
+        'service_id',
+        'status',
+        'start_time' ,
+        'end_time',
+    ];
+
+    public function employee(): BelongsTo
+    {
+        return $this->belongsTo(Employee::class);
+    }
+
+    public function customer(): BelongsTo
+    {
+        return $this->belongsTo(Customer::class);
+    }
+
+    public function pet(): BelongsTo
+    {
+        return $this->belongsTo(Pet::class);
+    }
+
+    public function service(): BelongsTo
+    {
+        return $this->belongsTo(Service::class);
+    }
+}
