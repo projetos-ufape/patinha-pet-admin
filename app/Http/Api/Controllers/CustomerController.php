a84f59785704de126da7f70c4e649d7c28e447a9
Luanzacarias <Luanzacarias@users.noreply.github.com>
2024-08-31 03:17:52 +0000

    style: autofix github actions

--- app/Http/Api/Controllers/CustomerController.php
+++ app/Http/Api/Controllers/CustomerController.php
@@ -2,64 +2,64 @@
 
 namespace App\Http\Api\Controllers;
 
-use Illuminate\Http\Request;
 use App\Http\Api\Requests\CustomerUpdateRequest;
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\DB;
 
 class CustomerController extends Controller
 {
+    public function index(Request $request)
+    {
 
-  public function index(Request $request) {
+        $user = $request->user();
+        $phone_number = $request->user()->customer->phone_number;
+        $address = $request->user()->address;
 
-    $user = $request->user();
-    $phone_number = $request->user()->customer->phone_number;
-    $address = $request->user()->address;
+        return response()->json(['user' => [
+            'name' => $user->name,
+            'email' => $user->email,
+            'cpf' => $user->cpf,
+            'phone_number' => $phone_number,
+            'address' => $address ? [
+                'cep' => $address->cep,
+                'city' => $address->city,
+                'state' => $address->state,
+                'street' => $address->street,
+                'district' => $address->district,
+                'number' => $address->number,
+                'complement' => $address->complement,
+            ] : null,
+        ]], 200);
 
-    return response()->json(['user' => [
-      'name' => $user->name,
-      'email' => $user->email,
-      'cpf' => $user->cpf,
-      'phone_number' => $phone_number,
-      'address' => $address ? [
-        'cep' => $address->cep,
-        'city' => $address->city,
-        'state' => $address->state,
-        'street' => $address->street,
-        'district' => $address->district,
-        'number' => $address->number,
-        'complement' => $address->complement,
-    ] : null
-    ]], 200);
-
-  }
-
-  public function update(CustomerUpdateRequest $request)
-  {
-    $data = $request->validated();
-    if (empty($data)) {
-      return response()->json([
-        'error' => 'Nenhum dado fornecido para atualização.'
-      ], 422);
     }
-    DB::transaction(function () use ($request, $data) {
-      $userData = Arr::except($data, ['phone_number', 'address', 'cpf', 'email']);
-      $request->user()->update($userData);
 
-      if (isset($data['address'])) {
-        if ($request->user()->address) {
-          $request->user()->address()->update($data['address']);
-        } else {
-          $request->user()->address()->create($data['address']);
+    public function update(CustomerUpdateRequest $request)
+    {
+        $data = $request->validated();
+        if (empty($data)) {
+            return response()->json([
+                'error' => 'Nenhum dado fornecido para atualização.',
+            ], 422);
         }
-      }
+        DB::transaction(function () use ($request, $data) {
+            $userData = Arr::except($data, ['phone_number', 'address', 'cpf', 'email']);
+            $request->user()->update($userData);
 
-      if (isset($data['phone_number'])) {
-        $request->user()->customer->update(['phone_number' => $data['phone_number']]);
-      }
-    });
+            if (isset($data['address'])) {
+                if ($request->user()->address) {
+                    $request->user()->address()->update($data['address']);
+                } else {
+                    $request->user()->address()->create($data['address']);
+                }
+            }
 
-    return response()->json(['message' => 'Atualizado com sucesso'], 200);
-  }
+            if (isset($data['phone_number'])) {
+                $request->user()->customer->update(['phone_number' => $data['phone_number']]);
+            }
+        });
+
+        return response()->json(['message' => 'Atualizado com sucesso'], 200);
+    }
 }
--- tests/Feature/Api/CustomerTest.php
+++ tests/Feature/Api/CustomerTest.php
@@ -5,164 +5,163 @@ use App\Models\User;
 use Laravel\Sanctum\Sanctum;
 
 it('can update a customer with only one attribute', function () {
-  $user = User::factory()->hasCustomer()->create([
-      'name' => 'Original Name',
-      'email' => 'original@example.com',
-  ]);
+    $user = User::factory()->hasCustomer()->create([
+        'name' => 'Original Name',
+        'email' => 'original@example.com',
+    ]);
 
-  Sanctum::actingAs($user, ['customer']);
+    Sanctum::actingAs($user, ['customer']);
 
-  $newName = 'Updated Name';
+    $newName = 'Updated Name';
 
-  $response = $this->patchJson(route('api.customers.update'), [
-      'name' => $newName,
-  ]);
+    $response = $this->patchJson(route('api.customers.update'), [
+        'name' => $newName,
+    ]);
 
-  $response->assertStatus(200)
-      ->assertJson(['message' => 'Atualizado com sucesso']);
+    $response->assertStatus(200)
+        ->assertJson(['message' => 'Atualizado com sucesso']);
 
-  $this->assertDatabaseHas('users', [
-      'id' => $user->id,
-      'name' => $newName,
-      'email' => 'original@example.com', 
-  ]);
+    $this->assertDatabaseHas('users', [
+        'id' => $user->id,
+        'name' => $newName,
+        'email' => 'original@example.com',
+    ]);
 
     $this->assertDatabaseHas('customers', [
-      'user_id' => $user->id,
-      'phone_number' => $user->customer->phone_number, 
-  ]);
+        'user_id' => $user->id,
+        'phone_number' => $user->customer->phone_number,
+    ]);
 });
 
 it('can not update a email', function () {
-  $user = User::factory()->hasCustomer()->create([
-      'name' => 'Original Name',
-      'email' => 'original@example.com',
-  ]);
+    $user = User::factory()->hasCustomer()->create([
+        'name' => 'Original Name',
+        'email' => 'original@example.com',
+    ]);
 
-  Sanctum::actingAs($user, ['customer']);
+    Sanctum::actingAs($user, ['customer']);
 
-  $newEmail = 'updated@example.com';
+    $newEmail = 'updated@example.com';
 
-  $response = $this->patchJson(route('api.customers.update'), [
-      'email' => $newEmail,
-      'name' => 'change Customer'
-  ]);
+    $response = $this->patchJson(route('api.customers.update'), [
+        'email' => $newEmail,
+        'name' => 'change Customer',
+    ]);
 
-  $response->assertStatus(200)
-      ->assertJson(['message' => 'Atualizado com sucesso']);
+    $response->assertStatus(200)
+        ->assertJson(['message' => 'Atualizado com sucesso']);
 
-  $this->assertDatabaseHas('users', [
-      'id' => $user->id,
-      'name' => 'change Customer',
-      'email' => $user->email,
-  ]);
+    $this->assertDatabaseHas('users', [
+        'id' => $user->id,
+        'name' => 'change Customer',
+        'email' => $user->email,
+    ]);
 
-  $this->assertDatabaseHas('customers', [
-      'user_id' => $user->id,
-      'phone_number' => $user->customer->phone_number,
-  ]);
+    $this->assertDatabaseHas('customers', [
+        'user_id' => $user->id,
+        'phone_number' => $user->customer->phone_number,
+    ]);
 });
 
 it('can not update a cpf', function () {
-  $user = User::factory()->hasCustomer()->create([
-      'name' => 'Original Name',
-      'email' => 'original@example.com',
-      'cpf' => '00000000000'
-  ]);
+    $user = User::factory()->hasCustomer()->create([
+        'name' => 'Original Name',
+        'email' => 'original@example.com',
+        'cpf' => '00000000000',
+    ]);
 
-  Sanctum::actingAs($user, ['customer']);
+    Sanctum::actingAs($user, ['customer']);
 
-  $newCpf = '11111111111';
+    $newCpf = '11111111111';
 
-  $response = $this->patchJson(route('api.customers.update'), [
-      'cpf' => $newCpf,
-      'name' => 'change Customer'
-  ]);
+    $response = $this->patchJson(route('api.customers.update'), [
+        'cpf' => $newCpf,
+        'name' => 'change Customer',
+    ]);
 
-  $response->assertStatus(200)
-      ->assertJson(['message' => 'Atualizado com sucesso']);
+    $response->assertStatus(200)
+        ->assertJson(['message' => 'Atualizado com sucesso']);
 
-  $this->assertDatabaseHas('users', [
-      'id' => $user->id,
-      'name' => 'change Customer',
-      'cpf' => $user->cpf,
-  ]);
+    $this->assertDatabaseHas('users', [
+        'id' => $user->id,
+        'name' => 'change Customer',
+        'cpf' => $user->cpf,
+    ]);
 
-  $this->assertDatabaseHas('customers', [
-      'user_id' => $user->id,
-      'phone_number' => $user->customer->phone_number,
-  ]);
+    $this->assertDatabaseHas('customers', [
+        'user_id' => $user->id,
+        'phone_number' => $user->customer->phone_number,
+    ]);
 });
 
 it('can update a customer with a full address', function () {
-  $user = User::factory()->hasCustomer()->create();
-  $address = Address::factory()->make()->toArray();
+    $user = User::factory()->hasCustomer()->create();
+    $address = Address::factory()->make()->toArray();
 
-  Sanctum::actingAs($user, ['customer']);
+    Sanctum::actingAs($user, ['customer']);
 
-  $response = $this->patchJson(route('api.customers.update'), [
-      'address' => $address,
-  ]);
+    $response = $this->patchJson(route('api.customers.update'), [
+        'address' => $address,
+    ]);
 
-  $response->assertStatus(200)
-      ->assertJson(['message' => 'Atualizado com sucesso']);
+    $response->assertStatus(200)
+        ->assertJson(['message' => 'Atualizado com sucesso']);
 
-  $this->assertDatabaseHas('addresses', [
-      'user_id' => $user->id,
-      'cep' => $address['cep'],
-      'street' => $address['street'],
-      'number' => $address['number'],
-      'district' => $address['district'],
-      'city' => $address['city'],
-      'complement' => $address['complement'],
-      'state' => $address['state'],
-  ]);
+    $this->assertDatabaseHas('addresses', [
+        'user_id' => $user->id,
+        'cep' => $address['cep'],
+        'street' => $address['street'],
+        'number' => $address['number'],
+        'district' => $address['district'],
+        'city' => $address['city'],
+        'complement' => $address['complement'],
+        'state' => $address['state'],
+    ]);
 });
 
 it('can update a customer with multiple attributes', function () {
-  $user = User::factory()->hasCustomer()->create();
+    $user = User::factory()->hasCustomer()->create();
 
-  Sanctum::actingAs($user, ['customer']);
+    Sanctum::actingAs($user, ['customer']);
 
-  $response = $this->patchJson(route('api.customers.update'), [
-      'name' => 'Updated Name',
-      'phone_number' => '09876543210',
-  ]);
+    $response = $this->patchJson(route('api.customers.update'), [
+        'name' => 'Updated Name',
+        'phone_number' => '09876543210',
+    ]);
 
-  $response->assertStatus(200)
-      ->assertJson(['message' => 'Atualizado com sucesso']);
+    $response->assertStatus(200)
+        ->assertJson(['message' => 'Atualizado com sucesso']);
 
-  $this->assertDatabaseHas('users', [
-      'id' => $user->id,
-      'name' => 'Updated Name',
-  ]);
+    $this->assertDatabaseHas('users', [
+        'id' => $user->id,
+        'name' => 'Updated Name',
+    ]);
 
-  $this->assertDatabaseHas('customers', [
-      'user_id' => $user->id,
-      'phone_number' => '09876543210',
-  ]);
+    $this->assertDatabaseHas('customers', [
+        'user_id' => $user->id,
+        'phone_number' => '09876543210',
+    ]);
 });
 
-
 it('fails to update a customer with no attributes provided', function () {
-  $user = User::factory()->hasCustomer()->create();
+    $user = User::factory()->hasCustomer()->create();
 
-  Sanctum::actingAs($user, ['customer']);
+    Sanctum::actingAs($user, ['customer']);
 
-  $response = $this->patchJson(route('api.customers.update'), []);
+    $response = $this->patchJson(route('api.customers.update'), []);
 
-  $response->assertStatus(422); 
+    $response->assertStatus(422);
 
-  $this->assertDatabaseHas('users', [
-      'id' => $user->id,
-      'name' => $user->name,
-      'email' => $user->email,
-  ]);
+    $this->assertDatabaseHas('users', [
+        'id' => $user->id,
+        'name' => $user->name,
+        'email' => $user->email,
+    ]);
 
-  $this->assertDatabaseHas('customers', [
-      'user_id' => $user->id,
-      'phone_number' => $user->customer->phone_number,
-  ]);
+    $this->assertDatabaseHas('customers', [
+        'user_id' => $user->id,
+        'phone_number' => $user->customer->phone_number,
+    ]);
 });
 
 test('Customer can be loaded', function () {
@@ -172,5 +171,5 @@ test('Customer can be loaded', function () {
     $response = $this->getJson(route('api.customers.index'));
 
     $response->assertStatus(200)
-        ->assertJsonStructure([ 'user' => ['name', 'email', 'cpf', 'phone_number', 'address']]);
+        ->assertJsonStructure(['user' => ['name', 'email', 'cpf', 'phone_number', 'address']]);
 });
