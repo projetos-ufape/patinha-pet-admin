20492429b414694112e78f9e968169661812b29c
AnaliceBattisti <AnaliceBattisti@users.noreply.github.com>
2024-09-05 18:50:18 +0000

    style: autofix github actions

--- app/Http/Controllers/SaleController.php
+++ app/Http/Controllers/SaleController.php
@@ -22,7 +22,7 @@ class SaleController extends Controller
     {
         $appointments = Appointment::paginate(15);
         $sales = Sale::with(['employee.user', 'customer.user', 'saleItem.productItem.product', 'saleItem.appointmentItem.appointment'])->paginate(15);
-    
+
         return view('comercial.index', [
             'sales' => $sales,
             'appointments' => $appointments,
--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,6 @@ Route::get('/dashboard', function () {
     return redirect()->route('comercial.index');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
